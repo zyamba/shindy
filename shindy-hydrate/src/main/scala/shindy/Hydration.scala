e2e32423e09f9dbf50559f7dc156e97a7a8d5c75
Ivan Luzyanin <ivanobulo@gmail.com>
2019-03-22 22:56:01 -0700

    add some documentation

--- shindy-hydrate/src/main/scala/shindy/Hydration.scala
+++ shindy-hydrate/src/main/scala/shindy/Hydration.scala
@@ -7,6 +7,12 @@ import shindy.EventSourced.EventHandler
 
 import scala.language.higherKinds
 
+/**
+  * Provides ability to hydrate state of the aggregate from event store, update and persist events in event store.
+  *
+  * @tparam STATE Type of state.
+  * @tparam EVENT Type of events.
+  */
 trait Hydration[STATE, EVENT] {
   def createNew[F[_] : Monad](
     sourcedCreation: SourcedCreation[STATE, EVENT, UUID]
@@ -16,4 +22,7 @@ trait Hydration[STATE, EVENT] {
     implicit eventHandler: EventHandler[STATE, EVENT],
     streamCompiler: fs2.Stream.Compiler[F, F],
   ): Hydrated[STATE, EVENT, Unit, F] = Hydrated.hydrate(aggregateId)
+
+  def persist[F[_], A](h: Hydrated[STATE, EVENT, A, F],
+    eventStore: EventStore[STATE, EVENT, F]): F[Either[String, (UUID, STATE, A)]] = h.persist(eventStore)
 }
